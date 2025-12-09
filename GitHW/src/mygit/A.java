feee627b40a3b820441cf4c613e5a84d3f5f2cbf
hyan1002 <1110nix62@gmail.com>
2025-12-09 11:08:57 +0900

    HW Alice branch

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -3,8 +3,8 @@ package mygit;
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println("This is A.java");
-		
+		System.out.println("This is A.java \n");
+		System.out.println("Switch Alice branch ");
 
 	}
 
--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -3,6 +3,7 @@ package mygit;
 public class C {
 	public void PlusC(String text) {
 		System.out.println("C" + text);
+		System.out.println("Alice branch");
 	}
 
 }
