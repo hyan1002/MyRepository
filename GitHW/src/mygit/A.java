ae684655047c0137a2d8346949d3b7f1a9b9c277
hyan1002 <1110nix62@gmail.com>
2025-12-10 09:16:38 +0900

    Bob branch A, B

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -4,7 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("This is A.java");
-		
+		System.out.println("Bob branch A.java");
 
 	}
 
--- GitHW/src/mygit/B.java
+++ GitHW/src/mygit/B.java
@@ -3,6 +3,7 @@ package mygit;
 public class B {
 	public void SayProperty() {
 		System.out.println("This is B.java");
+		System.out.println("Bob branch B.java");
 	}
 
 }
